e913b39c9e6b6498d22604c6f50c4750b1ca06c8
Damien Maillard <dmaillard06@gmail.com>
2016-04-01 09:58:42 +0200

    esli

--- index.js
+++ index.js
@@ -1,168 +1,178 @@
-(function(global){
+/* eslint-env browser, node */
 
-	// https://github.com/Polymer/URL/blob/master/url.js
-	// https://gist.github.com/Yaffle/1088850
-	function parseURL(url){
-		if( url == null ) throw new TypeError(url + ' is not a valid url');
-		if( url instanceof URL ) return url;
+(function(global) {
+    // https://github.com/Polymer/URL/blob/master/url.js
+    // https://gist.github.com/Yaffle/1088850
+    function parseURL(url) {
+        if (url === null) {
+            throw new TypeError(url + ' is not a valid url');
+        }
+        if (url instanceof URL ) { // eslint-disable-line
+            return url;
+        }
 
-		url = String(url);
-		url = url.replace(/^\s+|\s+$/g, ''); // trim
+        url = String(url);
+        url = url.replace(/^\s+|\s+$/g, ''); // trim
 
-		var regex = /^([^:\/?#]+:)?(?:\/\/(?:([^:@\/?#]*)(?::([^:@\/?#]*))?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/;
-		// /^([^:\/?#]+:)?(\/\/(?:[^:@\/?#]*(?::[^:@\/?#]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/;
-		var match = url.match(regex);
-		// authority = '//' + user + ':' + pass '@' + hostname + ':' port
-		var parsed = null;
+        var regex = /^([^:\/?#]+:)?(?:\/\/(?:([^:@\/?#]*)(?::([^:@\/?#]*))?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/;
+        // /^([^:\/?#]+:)?(\/\/(?:[^:@\/?#]*(?::[^:@\/?#]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/;
+        var match = url.match(regex);
+        // authority = '//' + user + ':' + pass '@' + hostname + ':' port
+        var parsed = null;
 
-		if( match ){
-			parsed = {
-				href     : match[0] || '',
-				protocol : match[1] || '',
-				username : match[2] || '',
-				password : match[3] || '',
-				host     : match[4] || '',
-				hostname : match[5] || '',
-				port     : match[6] || '',
-				pathname : match[7] || '',
-				search   : match[8] || '',
-				hash     : match[9] || ''
-			};
+        if (match) {
+            parsed = {
+                href: match[0] || '',
+                protocol: match[1] || '',
+                username: match[2] || '',
+                password: match[3] || '',
+                host: match[4] || '',
+                hostname: match[5] || '',
+                port: match[6] || '',
+                pathname: match[7] || '',
+                search: match[8] || '',
+                hash: match[9] || ''
+            };
 
-			if( parsed.protocol === 'file:' ){
-				parsed.origin = 'null';
-				parsed.host = '';
-			}
-			else{
-				parsed.origin = parsed.protocol;
-				if( parsed.protocol || parsed.host ) parsed.origin+= '//';
-				parsed.origin+= parsed.host;
-			}
-		}
-		else{
-			throw new RangeError();
-		}
+            if (parsed.protocol === 'file:') {
+                parsed.origin = 'null';
+                parsed.host = '';
+            } else {
+                parsed.origin = parsed.protocol;
+                if (parsed.protocol || parsed.host) {
+                    parsed.origin += '//';
+                }
+                parsed.origin += parsed.host;
+            }
+        } else {
+            throw new RangeError();
+        }
 
-		return parsed;
-	}
+        return parsed;
+    }
 
-	function removeDotSegments(input){
-		var output = [];
+    function removeDotSegments(input) {
+        var output = [];
 
-		input
-		.replace(/^(\.\.?(\/|$))+/, '')
-		.replace(/\/(\.(\/|$))+/g, '/')
-		.replace(/\/\.\.$/, '/../')
-		.replace(/\/?[^\/]*/g, function(p){
-			if( p === '/..' )
-				output.pop();
-			else
-				output.push(p);
-		});
+        input
+        .replace(/^(\.\.?(\/|$))+/, '')
+        .replace(/\/(\.(\/|$))+/g, '/')
+        .replace(/\/\.\.$/, '/../')
+        .replace(/\/?[^\/]*/g, function(p) {
+            if (p === '/..') {
+                output.pop();
+            } else {
+                output.push(p);
+            }
+        });
 
-		return output.join('').replace(/^\//, input.charAt(0) === '/' ? '/' : '');
-	}
+        return output.join('').replace(/^\//, input.charAt(0) === '/' ? '/' : '');
+    }
 
-	var URL = {
-		constructor: function(urlString, base){
-			var url = parseURL(urlString);
+    var URL = {
+        constructor: function(urlString, base) {
+            var url = parseURL(urlString);
 
-			if( arguments.length > 1 ){
-				base = parseURL(base);
-				var flag = url.protocol === '' && url.host === '' && url.username === '';
+            if (arguments.length > 1) {
+                base = parseURL(base);
+                var flag = url.protocol === '' && url.host === '' && url.username === '';
 
-				if( flag && url.pathname === '' && url.search === '' ){
-					url.search = base.search;
-				}
+                if (flag && url.pathname === '' && url.search === '') {
+                    url.search = base.search;
+                }
 
-				if( flag && url.pathname[0] !== '/' ){
-					var pathname = '';
+                if (flag && url.pathname[0] !== '/') {
+                    var pathname = '';
 
-					if( url.pathname ){
-						if( (base.host || base.username) && base.pathname === '' ) pathname+= '/';
-						pathname+= base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + url.pathname;
-					}
-					else{
-						pathname = base.pathname;
-					}
+                    if (url.pathname) {
+                        if ((base.host || base.username) && base.pathname === '') {
+                            pathname += '/';
+                        }
+                        pathname += base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + url.pathname;
+                    } else {
+                        pathname = base.pathname;
+                    }
 
-					url.pathname = pathname;
-				}
-				else if( base.protocol == 'file:' &&  urlString[0] === '/' && urlString[1] === '/' ){
-					url.pathname = '/' + url.host + url.pathname;
-					url.host = '';
-					url.hostname = '';
-				}
+                    url.pathname = pathname;
+                } else if (base.protocol === 'file:' && urlString[0] === '/' && urlString[1] === '/') {
+                    url.pathname = '/' + url.host + url.pathname;
+                    url.host = '';
+                    url.hostname = '';
+                }
 
-				url.pathname = removeDotSegments(url.pathname);
+                url.pathname = removeDotSegments(url.pathname);
 
-				if( flag ){
-					url.port = base.port;
-					url.hostname = base.hostname;
-					url.host = base.host;
-					url.password = base.password;
-					url.username = base.username;
-				}
+                if (flag) {
+                    url.port = base.port;
+                    url.hostname = base.hostname;
+                    url.host = base.host;
+                    url.password = base.password;
+                    url.username = base.username;
+                }
 
-				if( url.protocol === '' ){
-					url.protocol = base.protocol;
-				}
-				url.origin = base.origin;
-			}
+                if (url.protocol === '') {
+                    url.protocol = base.protocol;
+                }
+                url.origin = base.origin;
+            }
 
-			this.searchParams = new global.URLSearchParams();
+            this.searchParams = new global.URLSearchParams();
 
-			this.protocol = url.protocol;
-			this.username = url.username;
-			this.password = url.password;
-			this.host = url.host;
-			this.hostname = url.hostname;
-			this.origin = url.origin;
-			this.port = url.port;
-			this.pathname = url.pathname;
-			this.search = url.search;
-			this.hash = url.hash;
+            this.protocol = url.protocol;
+            this.username = url.username;
+            this.password = url.password;
+            this.host = url.host;
+            this.hostname = url.hostname;
+            this.origin = url.origin;
+            this.port = url.port;
+            this.pathname = url.pathname;
+            this.search = url.search;
+            this.hash = url.hash;
 
-			this.href = this.toString();
-		},
+            this.href = this.toString();
+        },
 
-		get search(){
-			var search = this.searchParams.toString();
-			if( search ){
-				search = '?' + search;
-			}
-			return search;
-		},
+        get search() {
+            var search = this.searchParams.toString();
+            if (search) {
+                search = '?' + search;
+            }
+            return search;
+        },
 
-		set search(value){
-			if( value[0] === '?' ) value = value.slice(1);
-			this.searchParams.fromString(value);
-		},
+        set search(value) {
+            if (value[0] === '?') {
+                value = value.slice(1);
+            }
+            this.searchParams.fromString(value);
+        },
 
-		toString: function(){
-			var url = '', search;
+        toString: function() {
+            var url = '';
 
-			url+= this.protocol;
-			url+= this.protocol === '' && this.host === '' ? '' : '//';
-			if( this.username ){
-				url+= this.username;
-				url+= this.password ? ':' + this.password : '';
-				url+= '@';
-			}
-			url+= this.host;
-			url+= this.pathname;
-			url+= this.search;
-			url+= this.hash;
+            url += this.protocol;
+            url += this.protocol === '' && this.host === '' ? '' : '//';
+            if (this.username) {
+                url += this.username;
+                url += this.password ? ':' + this.password : '';
+                url += '@';
+            }
+            url += this.host;
+            if (this.port) {
+                url += ':' + this.port;
+            }
+            url += this.pathname;
+            url += this.search;
+            url += this.hash;
 
-			return url;
-		}
-	};
+            return url;
+        }
+    };
 
-	URL.constructor.prototype = URL;
-	URL = URL.constructor;
+    URL.constructor.prototype = URL;
+    URL = URL.constructor;
 
-	if( false === 'URL' in global ){
-		global.URL = URL;
-	}
-
-})(typeof window != 'undefined' ? window : global);
+    if (('URL' in global) === false) {
+        global.URL = URL;
+    }
+})(typeof window === 'undefined' ? global : window);
